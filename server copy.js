8e7b16e4f17e86037cc3fbc7660ee67bd8d8c818
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-06-12 12:20:18 -0700

    all for now

--- server copy.js
+++ server copy.js
@@ -12,11 +12,11 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI/students)
 // mongoose.connect('mongodb://127.0.0.1:27017/students')
 const db = mongoose.connection
 db.once('open', () => {
-    console.log("Mongodb connection successful on port 3019")
+    console.log("Mongodb connection successful")
 })
 
 const userSchema = new mongoose.Schema({
@@ -26,18 +26,13 @@ const userSchema = new mongoose.Schema({
     branch: String
 })
 
-const Users = mongoose.model("info", userSchema)
-const Users2 = mongoose.model("name", userSchema)
+const Users = mongoose.model("data", userSchema)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'form.html'))
 })
 
-app.get('/form2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form2.html'))
-})
-
-app.post('/post', async (req, res, next) => {
+app.post('/post', async (req, res) => {
     const { regd_no, name, email, branch } = req.body
     const user = new Users({
         regd_no,
@@ -48,26 +43,6 @@ app.post('/post', async (req, res, next) => {
     await user.save()
     console.log(user)
     res.send("Form submission Succesful")
-    res.redirect('/form2');
-    
-    next();
-})
-
-app.post('/post2', async (req, res, next) => {
-    const { name} = req.body
-    // const { regd_no, name, email, branch } = req.body
-    const user = new Users2({
-        // regd_no,
-        name
-        // email,
-        // branch
-    })
-    await user.save()
-    console.log(user)
-    res.send("Form submission Succesful")
-    res.redirect('/form2');
-    
-    next();
 })
 
 
